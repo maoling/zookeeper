6fc0430617d4a344144b38d8a30ac67592a60d23
Nishanth Entoor <entoor.nishanth@gmail.com>
2020-04-24 04:15:52 +0200

    ZOOKEEPER-3800: improve the log printing the address when QuorumCnxManager#ListenerHandler's port binds

--- zookeeper-server/src/main/java/org/apache/zookeeper/server/quorum/QuorumCnxManager.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/quorum/QuorumCnxManager.java
@@ -1096,7 +1096,7 @@ public class QuorumCnxManager {
                             break;
                         }
 
-                        LOG.error("Exception while listening", e);
+                        LOG.error("Exception while listening on port {}", address, e);
 
                         if (e instanceof SocketException) {
                             socketException.set(true);
